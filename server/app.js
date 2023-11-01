7ae48d14adc3f537ac45e068334df8c48dda291e
constantinrazvan <razvanpana20@gmail.com>
2023-11-01 21:23:57 +0200

    init some controllers

--- server/app.js
+++ server/app.js
@@ -1,6 +1,32 @@
 import mongoose from 'mongoose';
 import express from 'express';
 
+//Auth Controllers
+import LoginController from './controllers/Auth/LoginController';
+import RegisterController from './controllers/Auth/RegsiterController'
+
+//Blog Post Controllers
+import CreatePostController from './controllers/Post/CreatePostController';
+import ReadPostController from './controllers/Post/ReadPostController';
+import UpdatePostController from './controllers/Post/UpdatePostController';
+import DeletePostController from './controllers/Post/DeletePostController';
+
 const app = express(); 
 const port = process.env.PORT || 4300;
+
 app.use(express.json());
+app.use(express.urlencoded());
+
+//AUTH APIs
+app.use("/api/loginUser", LoginController)
+app.use("/api/registerUser", RegisterController);
+
+//BLOG POST APIs
+app.use("/api/post/create", CreatePostController);
+app.use("/api/post/getAll", ReadPostController);
+app.use("/api/post/update", UpdatePostController);
+app.use("/api/post/delete/{_id}", DeletePostController)
+
+app.listen(port, () => { 
+    console.log(` Server is listening to ${port}`);
+})
---
+++ server/controllers/Post/ReadPostController.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const ReadPostController = async ( req, res ) => { 
+    
+}
+
+export default ReadPostController;
